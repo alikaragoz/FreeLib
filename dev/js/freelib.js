886b686147382cbc5b87dd975d4c41b6c98e0cc6
Ali <mail@alikaragoz.net>
2011-08-14 13:27:33 +0200

    Finished geolocation.

--- dev/js/freelib.js
+++ dev/js/freelib.js
@@ -1,5 +1,8 @@
 var freelib = (function() {
-
+	
+	// Scroller
+	var myScroll;
+	
     // Global variables
     var map = new Array();
     var db = null;
@@ -14,7 +17,32 @@ var freelib = (function() {
     var mapArgument = 'carto';
     var stationStatusArgument = 'stationdetails';
 
-    // Database initialisation
+	// Maximum distance for geoloc : 0.5km
+	var dist = 0.5;
+	
+	// Position watcher
+	var geoWatch;
+	
+	// Current position of the user
+	var curLatitude;
+	var curLongitude;
+	
+	// Initialisation function
+	function init () {
+		
+		// Init of the scroller
+		setTimeout(function () {
+			myScroll = new iScroll('wrapper');
+		}, 100);
+		
+		// For more accurate positionning 
+		geoWatcher();
+		
+		// We check if the database already exists
+		initDb();
+	}
+ 	
+	// Database initialisation
     function initDb() {
         try {
             if (window.openDatabase) {
@@ -35,7 +63,6 @@ var freelib = (function() {
         }
     }
 
-    // TODO : do some tests.
     // Database reset
     function resetDB() {
         if (window.openDatabase) {
@@ -44,6 +71,9 @@ var freelib = (function() {
                 tx.executeSql('DROP TABLE IF EXISTS map', [],
                 function() {});
             });
+        }
+		else {
+            console.log('Web Databases not supported');
         }
     }
 
@@ -57,6 +87,7 @@ var freelib = (function() {
         function(data) {
             xml = data
         })
+		// We do it in the complete because we have to make sure we have received the data
         .complete(function() {
             populateMapDB(xml.query.results.carto.markers.marker);
         })
@@ -69,14 +100,19 @@ var freelib = (function() {
     function populateMapDB(map) {
         // If the map is not empty
         if (map != null) {
-            db.transaction(function(tx) {
-                var i = 0
-                $(map).each(function() {
-                    tx.executeSql('INSERT INTO map (id, address, bonus, fullAddress, lat, lng, name, number, open) values (?, ?, ?, ?, ?, ?, ?, ?, ?)', [i, this.address, this.bonus, this.fullAddress, this.lat, this.lng, this.name, this.number, this.open]);
-                    i++;
-                });
-            });
-        }
+			if (db) {
+            	db.transaction(function(tx) {
+	                var i = 0
+	                $(map).each(function() {
+	                    tx.executeSql('INSERT INTO map (id, address, bonus, fullAddress, lat, lng, name, number, open) values (?, ?, ?, ?, ?, ?, ?, ?, ?)', [i, this.address, this.bonus, this.fullAddress, this.lat, this.lng, this.name, this.number, this.open]);
+	                    i++;
+	                });
+	            });
+	        }
+			else {
+				console.log('error occurred trying to open DB');
+			}
+		}
         else {
             console.log('An error occurred while loading the map');
         }
@@ -125,12 +161,152 @@ var freelib = (function() {
             });
 		} 
     }
-
+	
+	function geoWatcher() {
+		if (navigator.geolocation) {
+			geoWatch = navigator.geolocation.watchPosition(function(position){
+				curLatitude = position.coords.latitude;
+				curLongitude = position.coords.longitude;
+			},
+			// next function is the error callback
+			function (error) {
+				switch(error.code) {
+					case error.TIMEOUT:
+						alert ('Timeout');
+						break;
+					case error.POSITION_UNAVAILABLE:
+						alert ('Position unavailable');
+						break;
+					case error.PERMISSION_DENIED:
+						alert ('Permission denied');
+						break;
+					case error.UNKNOWN_ERROR:
+						alert ('Unknown error');
+						break;
+				}
+			},
+			{timeout:27000, maximumAge: 30000, enableHighAccuracy:true}
+			);
+		}
+	}
+	
+	function geolocate() {
+		if (navigator.geolocation) {
+			navigator.geolocation.clearWatch(geoWatch);
+			navigator.geolocation.getCurrentPosition(
+			function (position) {
+				var currentPosition = new LatLon(position.coords.latitude, position.coords.longitude);
+				searchPosition(currentPosition); 
+			}, 
+			// next function is the error callback
+			function (error) {
+				switch(error.code) {
+					case error.TIMEOUT:
+						alert ('Timeout');
+						break;
+					case error.POSITION_UNAVAILABLE:
+						alert ('Position unavailable');
+						break;
+					case error.PERMISSION_DENIED:
+						alert ('Permission denied');
+						break;
+					case error.UNKNOWN_ERROR:
+						alert ('Unknown error');
+						break;
+				}
+			},	
+			{timeout:27000, maximumAge: 30000, enableHighAccuracy:true}
+			);
+		}
+		else {
+			alert("La geolocalisation ne semble pas être supportée par votre navigateur");
+		}
+		
+		function searchPosition (currentPosition) {
+			if (window.openDatabase) {
+				db = openDatabase("Freelib", "1.0", "Votre velib dans la poche", 512000);
+	            db.transaction(function(tx) {
+					// Selecting all the row of the map table
+	                tx.executeSql('SELECT * FROM map', [],
+	                function(tx, results) {
+	                    if(results.rows && results.rows.length) {
+							// Our flag to know if there are available stations
+							var	stationsAround = 0;
+							
+							// The array witch will be transmitted to the getStationStatus function which retrives the stations status.
+							var stations = new Array();
+							
+							// We go through all the stations
+	                        for (i = 0; i < results.rows.length; i++) {
+								// Getting the position of the GPS
+								var stationPosition = new LatLon(results.rows.item(i)['lat'], results.rows.item(i)['lng']);
+								
+								// If the station is in a permieter of 500m we keep it
+								if (currentPosition.distanceTo(stationPosition) <= dist) {
+									stationsAround++;
+									
+									// We fill the unsorted array
+									stations.push([currentPosition.distanceTo(stationPosition),results.rows.item(i)]);
+								};			
+	                        }
+							// TODO : faire le style + <li> diff
+							if (stationsAround == 0) {
+								 $("#scroller").text("Aucune station à moins de 500m");
+							}
+							else {
+								// We sor the stations by the closest first
+								stations.sort();
+								
+								// Adding the elements in the list
+								$.each(stations, function() {
+									$("#scroller").append($('<li id="' + this[1]['number'] + '"><div class="location"><div class="adresse"><span id="velib_id">à ' + Math.round(this[0]*1000) + 'm</span>' + this[1]['fullAddress'] + '</div></div><div class="velib_status"><div class="velib_num"><div class="sign"></div><div class="flip"><div class="num1">?</div></div></div><div class="parks_num"><div class="sign">P</div><div class="flip"><div class="num2">?</div></div></div></div><div class="clr"></div></li>'));
+								});
+								
+								// Refreh the scroller with the new elements
+								myScroll.refresh();
+								
+								// Asynchronous update of the stations status
+								getStationsStatus(stations.sort());
+							}
+	                    }
+						else {
+		                    console.log('error occurred trying to open DB');
+		                }
+	                });
+	            });
+			} else {
+				 console.log('Web Databases not supported');
+			}
+		}
+	}
+	
+	function getStationsStatus(stations) {
+		$.each(stations, function() {
+			// Station element.
+			var station = this[1];
+			
+			// We build the YQL URL to fetch the status on velib's service
+ 			var stationURL = yqlURL + encodeURIComponent(yqlStatement) + encodeURIComponent(velibServiceURL + '/' + stationStatusArgument + '/' + station['number']) + yqlCallBack;
+ 			$.getJSON(
+	 			stationURL,
+	 			function(data) {
+					var available = data.query.results.station.available;
+					var free = data.query.results.station.free;
+					$('#' + station['number'] + ' .num1').text(available);
+					$('#' + station['number'] + ' .num2').text(free);
+				})
+	 			.error(function(){
+	 				console.log('Error while get the station status.');
+	 			});
+		});
+	}
+	
     return {
-        init: initDb,
+		init: init,
         reset: resetDB,
         getMap: getMap,
-        populateMapDB: populateMapDB,
-        searchStation: searchStation
+        searchStation: searchStation,
+		geolocate: geolocate,
+		getStationsStatus: getStationsStatus
     };
 })();
